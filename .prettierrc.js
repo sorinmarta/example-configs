f7d6b611cb4b098fc44229a47b91a80225efe1e1
Sorin Marta <sorinmarta0@gmail.com>
2024-03-24 23:35:38 +0200

    Added the rest of the configs

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+	trailingComma: 'all',
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	bracketSameLine: true,
+	plugins: ['@trivago/prettier-plugin-sort-imports'],
+	importOrder: [
+		'^@wordpress/(.*)$',
+		'<THIRD_PARTY_MODULES>',
+		'^[./]',
+	],
+	overrides: [
+		{
+			files: ['**/*.css', '**/*.html'],
+			options: {
+				singleQuote: false,
+			},
+		},
+	],
+};
